0fda90117adf21cc8c277060c9467766214738f9
andrey12v <andrey12v@gmail.com>
2022-02-14 20:47:18 -0600

    Add default rout and Redirect component
    https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/25600816#overview

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
@@ -7,12 +7,16 @@ import MainHeader from './components/MainHeader';
 
 //the switch component allows to utilize only one route at a time
 //weuse switch to match the full path instead of part of it 
+//We use Redirect component to point to what component the route should be redirected
 function App() {
   return (
     <div>
       <MainHeader />
       <main>
         <Switch>
+          <Route path='/' exact>
+            <Redirect to='/welcome' />
+          </Route>
           <Route path='/welcome'>
             <Welcome />
           </Route>
